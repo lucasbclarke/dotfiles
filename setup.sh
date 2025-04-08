d5d9664caeda64354fafb709c88ec28a666759c4
lucasbclarke <lucasbaileyclarke@outlook.com>
2025-04-09 08:06:04 +1000

    added ghostty to setup file, removed firefox, added brave, added other git repos and changed ordering

--- setup.sh
+++ setup.sh
@@ -1,10 +1,9 @@
 sudo apt install zsh
 sudo apt install tmux
-sudo install -d -m 0755 /etc/apt/keyrings
-wget -q https://packages.mozilla.org/apt/repo-signing-key.gpg -O- | sudo tee /etc/apt/keyrings/packages.mozilla.org.asc > /dev/null
-echo "deb [signed-by=/etc/apt/keyrings/packages.mozilla.org.asc] https://packages.mozilla.org/apt mozilla main" | sudo tee -a /etc/apt/sources.list.d/mozilla.list > /dev/null
-sudo apt-get update && sudo apt-get install firefox
-sudo apt install git
-git clone https://github.com/LearnerCode-01/code
 sudo apt install curl
+sudo apt install git
+sudo apt install ghostty
+curl -fsS https://dl.brave.com/install.sh | sh
+git clone https://github.com/lucasbclarke/code
+git clone https://github.com/lucasbclarke/dotfiles
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
